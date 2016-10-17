035ef443111005c1ea748d2a68d8ec205e3642dd
Dimitris Klisiaris <dklisiaris@gmail.com>
2016-10-18 01:23:42 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Ghostbot
+
+![Ghostbot logo](https://github.com/dklisiaris/ghostbot/blob/master/ghostbot-logo.png?raw=true)
+
+Ghostbot is a minimalistic AJAX driven theme focused on clean typography for the [Ghost](http://github.com/tryghost/ghost/) blogging platform. It is based on [Ghostwriter](https://github.com/roryg/ghostwriter) and inspired by [Robots](https://robots.thoughtbot.com/).
+
+**[Ghostbot Demo](http://ghostbot.klisiaris.org)**
+
+**[Download Ghostbot](https://github.com/dklisiaris/ghostbot/archive/master.zip)**
+
+### Screenshots
+
+![Ghostbot index page](https://github.com/dklisiaris/ghostbot/blob/master/ghostbot-index.png?raw=true)
+
+![Ghostbot post page](https://github.com/dklisiaris/ghostbot/blob/master/ghostbot-post.jpg?raw=true)
+
+### Features
+
+* Clean typography focused design.
+* Fully responsive (includes fitvid.js for responsive video embeds).
+* AJAX loading for fast, smooth transitions between posts and pages.
+* SASS files included.
+* Static page support.
+* Featured post support.
---
+++ assets/js/scripts.js
@@ -0,0 +1,182 @@
+jQuery(function($) {
+
+  /* ============================================================ */
+  /* Syntax Highlighting */
+  /* ============================================================ */
+  hljs.initHighlightingOnLoad();
+
+  /* ============================================================ */
+  /* Responsive Videos */
+  /* ============================================================ */
+
+  $(".post-content").fitVids();
+
+  /* ============================================================ */
+  /* Scroll To Top */
+  /* ============================================================ */
+
+  $('.js-jump-top').on('click', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      'scrollTop': 0
+    });
+  });
+
+  /* ============================================================ */
+  /* Ajax Loading */
+  /* ============================================================ */
+
+  var History = window.History;
+  var loading = false;
+  var showIndex = false;
+  var $ajaxContainer = $('#ajax-container');
+  var $latestPost = $('#latest-post');
+  var $postIndex = $('#post-index');
+  var $topNav = $('#top-navigation');
+  var $siteNav = $('#site-navigation');
+
+  var showTopNav = function() {
+    $siteNav.hide();
+    $topNav.show();
+    console.log('top nav appears');
+  }
+
+  var showSiteNav = function() {
+    $topNav.hide();
+    $siteNav.show();
+  }
+
+  // Initially hide the index and show the latest post
+  // $latestPost.show();
+  // $postIndex.hide();
+
+  // Initially hide the latest post and show the index
+  $latestPost.hide();
+  $postIndex.show();
+  showSiteNav();
+
+  // Show the index if the url has "page" in it (a simple
+  // way of checking if we're on a paginated page.)
+  if (window.location.pathname.indexOf('page') === 1 || window.location.pathname === '/') {
+    $latestPost.hide();
+    $postIndex.show();
+    showSiteNav();
+  } else {
+    showTopNav();
+  }
+
+  // Check if history is enabled for the browser
+  if (!History.enabled) {
+    return false;
+  }
+
+  History.Adapter.bind(window, 'statechange', function() {
+    var State = History.getState();
+
+    // Get the requested url and replace the current content
+    // with the loaded content
+    $.get(State.url, function(result) {
+      var $html = $(result);
+      var $newContent = $('#ajax-container', $html).contents();
+
+      // Set the title to the requested urls document title
+      document.title = $html.filter('title').text();
+
+      $('html, body').animate({
+        'scrollTop': 0
+      });
+
+      $ajaxContainer.fadeOut(500, function() {
+        $latestPost = $newContent.filter('#latest-post');
+        $postIndex = $newContent.filter('#post-index');
+
+        if (showIndex === true) {
+          $latestPost.hide();
+          showSiteNav();
+        } else {
+          $latestPost.show();
+          $postIndex.hide();
+          showTopNav();
+        }
+
+        // Re run fitvid.js
+        $newContent.fitVids();
+
+        $ajaxContainer.html($newContent);
+        $ajaxContainer.fadeIn(500);
+
+        NProgress.done();
+
+        loading = false;
+        showIndex = false;
+      });
+    }).fail(function() {
+      // Request fail
+      NProgress.done();
+      location.reload();
+    });
+  });
+
+  $('body').on('click', '.js-ajax-link, .pagination a, .post-tags a, .post-header a', function(e) {
+    e.preventDefault();
+
+    if (loading === false) {
+      var currentState = History.getState();
+      var url = $(this).attr('href');
+      var title = $(this).attr('title') || null;
+
+      //if url starts with http:// and currentState.url starts with
+      // https://, replace the protocol in url
+      if (url.indexOf("http://", 0) === 0) {
+        var urlNoProt = url.replace(/.*?:\/\//g, "");
+        var curProt = currentState.url.split("/")[0];
+        url = curProt + "//" + urlNoProt;
+      }
+
+      // If the requested url is not the current states url push
+      // the new state and make the ajax call.
+      if (url !== currentState.url.replace(/\/$/, "")) {
+        loading = true;
+
+        // Check if we need to show the post index after we've
+        // loaded the new content
+        if ($(this).hasClass('js-show-index') || $(this).parent('.pagination').length > 0) {
+          showIndex = true;
+        }
+
+        NProgress.start();
+
+        History.pushState({}, title, url);
+      } else {
+        // Swap in the latest post or post index as needed
+        if ($(this).hasClass('js-show-index')) {
+          $('html, body').animate({
+            'scrollTop': 0
+          });
+
+          NProgress.start();
+
+          $latestPost.fadeOut(300, function() {
+            $postIndex.fadeIn(300);
+            showSiteNav();
+            NProgress.done();
+          });
+        } else {
+          $('html, body').animate({
+            'scrollTop': 0
+          });
+
+          NProgress.start();
+
+          $postIndex.fadeOut(300, function() {
+            $latestPost.fadeIn(300);
+            showTopNav();
+            NProgress.done();
+          });
+        }
+      }
+    }
+  });
+
+});
